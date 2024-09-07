55071d4b86c2b3a08e23431dbdc4b9eeb3b60410
Pbluer <ramonpvlima2@hotmail.com>
2024-09-07 13:06:02 -0300

    Finish project / Projeto finalizado

--- lib/ui/hom_page.dart
+++ lib/ui/hom_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -21,7 +22,7 @@ class _HomeAppState extends State<HomeApp> {
     if( _search == null ){
       response = await http.get(Uri.parse('https://api.giphy.com/v1/gifs/trending?api_key=MMrynW5F4avbqJMZGTjVSyoLYohWzeCz&limit=20&offset=0&rating=g&bundle=messaging_non_clips'));
     }else{
-      response = await http.get(Uri.parse('https://api.giphy.com/v1/gifs/search?api_key=MMrynW5F4avbqJMZGTjVSyoLYohWzeCz&q=$_search&limit=20&offset=0&rating=g&lang=en&bundle=messaging_non_clips'));
+      response = await http.get(Uri.parse('https://api.giphy.com/v1/gifs/search?api_key=MMrynW5F4avbqJMZGTjVSyoLYohWzeCz&q=$_search&limit=19&offset=$_offset&rating=g&lang=en&bundle=messaging_non_clips'));
     }
 
 
@@ -67,6 +68,7 @@ class _HomeAppState extends State<HomeApp> {
                     _search = null;
                   }else{
                     _search = texto;
+                    _offset = 0;
                   }
                 });
               },
@@ -102,7 +104,11 @@ class _HomeAppState extends State<HomeApp> {
   }
 
   int _getCount(List data){
-    if( _search )
+    if( _search == null ){
+      return data.length;
+    }else{
+      return data.length + 1;
+    }
   }
 
   Widget _createGifTable(BuildContext context,AsyncSnapshot snap){
@@ -113,13 +119,43 @@ class _HomeAppState extends State<HomeApp> {
         crossAxisSpacing: 10.0,
         mainAxisSpacing: 10.0,
       ),
-      itemCount: snap.data['data'].length,
+      itemCount: _getCount( snap.data['data'] ),
       itemBuilder: (context,index){
-        return GestureDetector(
-          child: Image.network( snap.data['data'][index]['images']['fixed_height_small']['url'],
-          height: 300,
-          fit: BoxFit.cover,),
-        );
+
+        if( _search == null || index < snap.data['data'].length ){
+          return GestureDetector(
+            child: Image.network( snap.data['data'][index]['images']['fixed_height_small']['url'],
+              height: 300,
+              fit: BoxFit.cover,),
+            onTap: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GifPage(snap.data['data'][index]) )
+              );
+            },
+          );
+        }else{
+          return Container(
+            child: GestureDetector(
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon( Icons.add, color: Colors.white, size:70.0 ),
+                  Text(
+                    'Carregar mais...',
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  )
+                ],
+              ),
+              onTap: (){
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            ),
+          );
+        }
+
       }
   );
   }
